096fce8e8e780959f83f0bcefa48221d13c91935
Jose Alvarez C <alvarez.jo.developer@gmail.com>
2018-03-08 23:36:23 -0800

    Lifted state of Squares up to the a single state in the Board. Important technique.
    "When you want to aggregate data from multiple children or to have two child components communicate with each other, move the state upwards so that it lives in the parent component. The parent can then pass the state back down to the children via props, so that the child components are always in sync with each other and with the parent."
    
    "Let’s recap what happens here:
    
        The onClick prop on the built-in DOM <button> component tells React to set up a click event listener.
        When the button is clicked, React will call the onClick event handler defined in Square’s render() method.
        This event handler calls this.props.onClick(). Square’s props were specified by the Board.
        Board passed onClick={() => this.handleClick(i)} to Square, so, when called, it runs this.handleClick(i) on the Board.
        We have not defined the handleClick() method on the Board yet, so the code crashes."

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,29 +2,79 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+
+//Square no longer keeps its own state; it receives its 
+//value from its parent Board and informs its parent when 
+//it’s clicked. We call components like this 
+//controlled components.
 class Square extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
+    // We are not using the state property of Square
+    // Or "it doesn't have a state anymore"
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         value: null,
+    //     };
+    // }
 
 
     render() {
         return (
-            <button className="square" onClick={()=>this.setState({value:'X'})}>
-            {this.state.value}
+            //Now when the Square is clicked, it will call the onClick function that was passed through by Board.
+            // The onClick prop on the Built-in DOM <button> tells React to set up a click event listener.
+            // When the button is clicked, React will call the onClick event handler defined in Square's render() method.
+                // "Specified"? Function was defined somewhere else.
+            // This event handler calls this.props.onClick(). Sauare's props were specified by the Board.
+            // Board passed onClick = {() => this.handleClick(i)} tp SqUare, so, 
+                // when called, it runs this.handleClick(i) on the Board.
+                // notice *****(i)*****
+            <button className="square" onClick={()=>this.props.onClick()}>
+            {this.props.value}
             </button>
         );
+        //Note that DOM <button> element’s onClick attribute has a special meaning to React, 
+        //but we could have named Square’s onClick prop or Board’s handleClick method differently. 
+        //It is, however, conventional in React apps to use on* names for the attributes and handle* 
+        //for the handler methods.
     }
 }
 
-
+/*
+When you want to aggregate data from multiple children 
+or to have two child components communicate with each other,
+move the state upwards so that it lives in the parent 
+component. The parent can then pass the state back down to 
+the children via props, so that the child components are 
+always in sync with each other and with the parent.
+*/
 class Board extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();// was slick needed?
+        // We call .slice() to copy the squares array instead of mutating the existing array.
+        squares[i] = 'X';
+        this.setState({squares:squares}); // update...
+    }
+
     renderSquare(i) {
-        return <Square value={i} />;
+    // Passing two important prompts from Board to Square
+    return (
+        <Square 
+            value={this.state.squares[i]} 
+            // Board passed onClick = {() => this.handleClick(i)} tp SqUare, so, 
+                // when called, it runs this.handleClick(i) on the Board.
+                // notice *****(i)*****
+            onClick={() => this.handleClick(i)}
+            />
+        );
     }
 
     render() {
