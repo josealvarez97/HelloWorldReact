776f137c0b978e29d54b87834d9585fb87cff3e2
Jose Alvarez C <alvarez.jo.developer@gmail.com>
2018-03-10 11:50:12 -0800

    Created map that maps steps to moves and returns a JS object / React component for it
    "For each step in the history, we create a list item <li> with a button <button> inside it that has a click handler which we’ll implement shortly. With this code, you should see a list of the moves that have been made in the game, along with a warning that says:
    
        Warning: Each child in an array or iterator should have a unique “key” prop. Check the render method of “Game”."
    
    "Let’s talk about what that warning means."
    
    "index.js:2178 Warning: Each child in an array or iterator should have a unique "key" prop.
    
    Check the render method of `Game`. See https://fb.me/react-warning-keys for more information.
        in li (at index.js:185)
        in Game (at index.js:246)"

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -176,7 +176,17 @@ class Game extends React.Component {
         const history = this.state.history;
         const current = history[history.length - 1];
 
-        const winner = calculateWinner(current.squares);
+        // Map array of data
+        const moves = history.map((step, move) => {
+            const desc = move ?
+                'Go to move #' + move :
+                'Go to game start';
+            return (
+                <li>
+                    <button onClick={()=>this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
 
         let status;
         const winner = calculateWinner(current.squares);
@@ -196,7 +206,7 @@ class Game extends React.Component {
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
